d74f061b50139763cf29cd7e14216c3a8e5b9cca
Haider <haideralirafeeq@gmail.com>
2023-11-29 12:45:40 +0500

    map basic done

--- Map_Filter_Foreach.js
+++ Map_Filter_Foreach.js
@@ -20,69 +20,79 @@
                   //  filter
 
 
-let books = [
-    {
-        title: "The Catcher in the Rye",
-        genre: "Fiction",
-        publishDate: "July 16, 1951"
-    },
-    {
-        title: "To Kill a Mockingbird",
-        genre: "Classic",
-        publishDate: "July 11, 1960"
-    },
-    {
-        title: "1984",
-        genre: "Dystopian",
-        publishDate: "June 8, 1949"
-    },
-    {
-        title: "The Great Gatsby",
-        genre: "Fiction",
-        publishDate: "April 10, 1925"
-    },
-    {
-        title: "Harry Potter and the Sorcerer's Stone",
-        genre: "Fantasy",
-        publishDate: "June 26, 1997"
-    },
-    {
-        title: "The Hobbit",
-        genre: "Fantasy",
-        publishDate: "September 21, 1937"
-    },
-    {
-        title: "The Lord of the Rings",
-        genre: "Fantasy",
-        publishDate: "July 29, 1954"
-    },
-    {
-        title: "Pride and Prejudice",
-        genre: "Romance",
-        publishDate: "January 28, 1813"
-    },
-    {
-        title: "The Da Vinci Code",
-        genre: "Mystery",
-        publishDate: "March 18, 2003"
-    },
-    {
-        title: "The Hitchhiker's Guide to the Galaxy",
-        genre: "Science Fiction",
-        publishDate: "October 12, 1979"
-    },
-    {
-        title: "flowers",
-        genre: "Romance",
-        publishDate: "January 29, 2015"
-    },
-];
+// let books = [
+//     {
+//         title: "The Catcher in the Rye",
+//         genre: "Fiction",
+//         publishDate: "July 16, 1951"
+//     },
+//     {
+//         title: "To Kill a Mockingbird",
+//         genre: "Classic",
+//         publishDate: "July 11, 1960"
+//     },
+//     {
+//         title: "1984",
+//         genre: "Dystopian",
+//         publishDate: "June 8, 1949"
+//     },
+//     {
+//         title: "The Great Gatsby",
+//         genre: "Fiction",
+//         publishDate: "April 10, 1925"
+//     },
+//     {
+//         title: "Harry Potter and the Sorcerer's Stone",
+//         genre: "Fantasy",
+//         publishDate: "June 26, 1997"
+//     },
+//     {
+//         title: "The Hobbit",
+//         genre: "Fantasy",
+//         publishDate: "September 21, 1937"
+//     },
+//     {
+//         title: "The Lord of the Rings",
+//         genre: "Fantasy",
+//         publishDate: "July 29, 1954"
+//     },
+//     {
+//         title: "Pride and Prejudice",
+//         genre: "Romance",
+//         publishDate: "January 28, 1813"
+//     },
+//     {
+//         title: "The Da Vinci Code",
+//         genre: "Mystery",
+//         publishDate: "March 18, 2003"
+//     },
+//     {
+//         title: "The Hitchhiker's Guide to the Galaxy",
+//         genre: "Science Fiction",
+//         publishDate: "October 12, 1979"
+//     },
+//     {
+//         title: "flowers",
+//         genre: "Romance",
+//         publishDate: "January 29, 2015"
+//     },
+// ];
 
-let userbooks = books.filter( (bk) => {
+// let userbooks = books.filter( (bk) => {
     // return bk.publishDate >= 2000;
-    
+
     // Convert the publishDate string to a Date object
-    const publishDate = new Date(bk.publishDate);
-    return publishDate >= new Date("January 1, 2000");
-})
-console.log(userbooks);
+//     const publishDate = new Date(bk.publishDate);
+//     return publishDate >= new Date("January 1, 2000");
+// })
+// console.log(userbooks);
+
+
+// Map
+
+const num = [1,2,3,4,5,6,7,8,9,10]
+ let newnum = num.map( (num) => {
+    return num + 2
+ })
+
+ console.log(newnum);
